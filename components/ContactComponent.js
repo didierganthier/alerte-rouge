6d40c6d72ce3a3dd5f7f20cf3e3e553fc90a8d87
amiraldany <augustindany@hotmail.com>
2022-01-29 16:57:52 -0500

    Footer Fixed, Links and text edited

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -6,36 +6,34 @@ function ContactComponent(){
         <div className="contact-container-bg">
             <Container className="contact-container">
                 <Row>
-                    <Col xs={12} md={6}>
+                    <Col xs={17} md={6}>
                         <h1>Contactez Nous</h1>
                         <p>
-                            N'hésitez pas à nous contacter si vous avez des questions, appelez le +50931077373. 
-                            ou ecrivez a l'adresse: contact@alerterouge.org 
-                            L'équipe Alerte Rouge est disponible pour vous servir.
+                          N'hésitez pas à nous contacter si vous avez des questions, ou des suggestions
+                          Contactez-nous <b>+50931077373 </b> ou par email: <b>contact@alerterouge.org </b> <br/>
+                            L'équipe Alerte Rouge sera toujours disponible pour vous servir.
                         </p>
+                        
                         <div> 
                         
-                          <a href= "https://www.facebook.com/profile.php?id=100075500118346"><span className="fa fa-facebook"> <img src="assets/facebook.png" width={30} height={30}/></span></a> 
-                            <a href= "https://www.instagram.com/alerterougeht/"> <span className="fa fa-instagram"><img src="assets/instagram.png" width={30} height={30}/></span></a>
-                            <a href="https://twitter.com/alerterougeht"><span className="fa fa-twitter"><img src="assets/twitter.png" width={30} height={30}/></span> </a> 
+                          <a href= "https://www.facebook.com/profile.php?id=100075500118346" target="_blank" ><span className="fa fa-facebook"> <img src="assets/facebook.png" width={30} height={30}/></span></a> 
+                            <a href= "https://www.instagram.com/alerterougeht/" target="_blank" > <span className="fa fa-instagram"><img src="assets/instagram.png" width={30} height={30}/></span></a>
+                            <a href="https://twitter.com/alerterougeht" target="_blank" ><span className="fa fa-twitter"><img src="assets/twitter.png" width={30} height={30}/></span> </a> 
                         </div>
+
                     </Col>
-                    <Col xs={12} md={3}>
-                        <b>Links</b>
+
+                </Row>
+                <Row>
+                    <Col xs={14} >
+                        <b>Liens Utiles</b>
                         <br/>
                         <br/>
-                        <h6>Overview</h6>
-                        <h6>Social Media</h6>
-                        <h6>Contact</h6>
+                        <h6>Formulaire</h6>
+                        <h6>Planifier un Don</h6>
+                        <h6>Passer le test d'eligibilite</h6>
                     </Col>
-                    <Col xs={12} md={3}>
-                        <b>Links</b>
-                        <br/>
-                        <br/>
-                        <h6>Overview</h6>
-                        <h6>Social Media</h6>
-                        <h6>Contact</h6>
-                    </Col>
+    
                 </Row>
             </Container>
         </div>
